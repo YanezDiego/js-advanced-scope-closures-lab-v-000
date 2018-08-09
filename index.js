ea8d72f5faf24bcf7e1925c127c8426b052100fc
Diego Yanez <diegoyanez@MacBook-Pro-5.local>
2018-08-09 13:47:22 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function produceDrivingRange(blockRange){
+  return function(start, end){
+    const distance = parseInt(end) - parseInt(start)
+    const difference = Math.abs(blockRange - distance)
+
+    if(distance > blockRange){
+      return `${difference} blocks out of range`
+    } else if (distance < blockRange) {
+      return `within range by ${difference}`
+    }
+  }
+}
+
+function produceTipCalculator (percent){
+  return function(amount){
+    return amount * percent
+  }
+}
+
+
+function createDriver(){
+  let DriverId = 0
+  return class{
+      constructor (name){
+        this.name = name
+        this.id = ++DriverId
+      }
+    }
+}
